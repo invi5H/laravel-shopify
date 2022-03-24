73bf07af292e414ab70908c4b3ad5fa0ba9a8f34
Mridul Aggarwal <mridul89@users.noreply.github.com>
2022-03-24 15:19:03 +0530

    move macros to service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Invi5h\LaravelShopify;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 use SocialiteProviders\Manager\SocialiteWasCalled;
@@ -35,5 +36,36 @@ class ServiceProvider extends LaravelServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../routes/laravelshopify.php');
 
         Event::listen(SocialiteWasCalled::class, ShopifyExtendSocialite::class);
+
+        $this->collectionMacros();
+    }
+
+    public function collectionMacros() : void
+    {
+        Collection::macro('trim', function () {
+            /** @var Collection $this */
+            return $this->map(fn($value) => is_string($value) ? trim($value) : $value);
+        });
+        Collection::macro('trimRecursive', function () {
+            /** @var Collection $this */
+            return $this->map(function ($value) {
+                if (is_string($value)) {
+                    return trim($value);
+                }
+                if (is_array($value)) {
+                    return collect($value)->trimRecursive()->all();
+                }
+                if ($value instanceof Collection) {
+                    return $value->trimRecursive();
+                }
+
+                return $value;
+            });
+        });
+        Collection::macro('mapKeys', function (callable $callback) {
+            /** @var Collection $this */
+            /* @psalm-suppress InvalidArgument */
+            return $this->keys()->map($callback)->combine($this->values());
+        });
     }
 }
