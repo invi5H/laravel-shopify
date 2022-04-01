428edeb721eb9a722a1e5c0d754526aba4719187
Mridul Aggarwal <mridul89@users.noreply.github.com>
2022-04-01 18:27:49 +0530

    complete test of config

--- tests/Unit/ConfigTest.php
+++ tests/Unit/ConfigTest.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Eloquent\Model;
 use Invi5h\LaravelShopify\Contracts\ShopModelInterface;
+use Invi5h\LaravelShopify\Models\AppType;
+use Invi5h\LaravelShopify\Models\Billing\BillingType;
 
 it('is valid eloquent model', function () : void {
     $class = config('laravelshopify.shop_model');
@@ -10,4 +12,14 @@ it('is valid eloquent model', function () : void {
     $object = new $class();
     expect($object)->toBeInstanceOf(Model::class);
     expect($object)->toBeInstanceOf(ShopModelInterface::class);
+});
+
+it('is valid app type', function () : void {
+    $type = config('laravelshopify.type');
+    expect($type)->toBeInstanceOf(AppType::class);
+});
+
+it('is valid billing type', function () : void {
+    $type = config('laravelshopify.billing_type');
+    expect($type)->toBeInstanceOf(BillingType::class);
 });
